b309c8ad3416edbca34576a8ecec037f208cff56
Niranjan P <niranjan.ece1@gmail.com>
2017-01-09 17:31:33 +0530

    improvements

--- app/src/main/java/app/ar/vr/com/myarvrapp/Constants.java
+++ app/src/main/java/app/ar/vr/com/myarvrapp/Constants.java
@@ -6,5 +6,5 @@ package app.ar.vr.com.myarvrapp;
 
 public class Constants {
 
-    public static String VUFORIA_LICENSE_KEY = "Af1JPnv/////AAAAGXARuOWfJE2vveQmLU/SiIkLFi1zp7STI8MfilbXytSnmrhffVt2Zwns24vNwX60fXmQtWfxPPmB29miK/vV1IRUhfYhlqizbb2/agYYLtPsfiiK57S6nx8yRbfGewXK13F8JpRvqhmAdIT00B9fX8r72ZmDDS+jJiyuFLs0pFjqI6cY0Oud2w+k01maODVK+WGpL00o3LKD7mkcs+j5aodSfCj1ZkMgtQn99lI0Iybk29nU7qeOc0pOo4viSS5xARphKd4UV3FpIPcUp14DNuBlboHb+YYj3n2rHo04D1GZw/2cOM6P2R/eJF7WYHJ2yv5t56KETcJl1WXn3UUegJF41luAy79wxSXNsozdjST3";
+    public static String VUFORIA_LICENSE_KEY = "";
 }
